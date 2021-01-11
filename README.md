9b0339b9a3058420cb0eba3d4cff2804900a5548
tyautyau56 <i19yanagi@gmail.com>
2021-01-11 17:17:14 +0900

    change README.txt to README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+## Deno practice
+
+I did a tutorial on [this](https://youtube.com/playlist?list=PL4cUxeGkcC9gnaJdxuGvEGYQ9iHb8mxsh) one.
--- editFile.ts
+++ editFile.ts
@@ -3,16 +3,16 @@
 // const data = await Deno.readFile('README.txt')
 // console.log(decoder.decode(data))
 // reading file other example
-const data = await Deno.readTextFile('readme.txt')
+const data = await Deno.readTextFile('README.md')
 console.log(data)
 
 // writing file
 // const encoder = new TextEncoder();
 // const text = 'hello world'
-// await Deno.writeFile('readme.txt', encoder.encode(text))
+// await Deno.writeFile('README.md', encoder.encode(text))
 
-const text = await Deno.writeTextFile('readme.txt', "hello world")
+const text = await Deno.writeTextFile('README.md', "hello world")
 
 // renaming file and removing file
-// await Deno.rename('readme.txt', 'README.txt')
-// await Deno.remove('readme.txt')
+// await Deno.rename('README.md', 'README.txt')
+// await Deno.remove('README.md')
