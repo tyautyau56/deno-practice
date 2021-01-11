3c2535e1cd79bdfb075e9e253189fddb8d2c2f11
tyautyau56 <i19yanagi@gmail.com>
2021-01-11 17:05:24 +0900

    change denon.config.ts

--- denon.config.ts
+++ denon.config.ts
@@ -2,7 +2,7 @@ import { DenonConfig } from "https://deno.land/x/denon/mod.ts";
 
 const config: DenonConfig = {
     scripts: {
-        dev: "deno run --allow-net --allow-read --allow-write app.ts",
+        dev: "deno run --allow-net --allow-read --allow-write api/app.ts",
     },
 };
 
